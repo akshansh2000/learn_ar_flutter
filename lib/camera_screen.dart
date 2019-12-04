59ba36ab90a5ecd9284634cba2b78987f489abac
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-12-05 01:51:05 +0530

    tracking camera screen

---
+++ lib/camera_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  ArCoreController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

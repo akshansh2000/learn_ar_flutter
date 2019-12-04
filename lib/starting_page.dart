ee66651806e586f2b44f2b05700990d4fa0be7f9
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-12-05 01:15:40 +0530

    added start screen

--- lib/starting_page.dart
+++ lib/starting_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:learn_ar_flutter/strings.dart' as strings;
+
 class StartingPage extends StatelessWidget {
   const StartingPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FlatButton(
+          child: Text(
+            strings.start.toUpperCase(),
+            style: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 40,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          onPressed: null,
+        ),
+      ),
+    );
   }
 }

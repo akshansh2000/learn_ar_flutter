2c442bf28525e1a28c04443cd25e2a7a870a2f5f
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-01-16 11:11:06 +0530

    modified about box

--- lib/about_widget.dart
+++ lib/about_widget.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'package:learn_ar_flutter/strings.dart' as strings;
+
 class AboutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.lightBlue[50],
+      backgroundColor: Colors.deepPurple,
+      contentTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+      ),
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20.0))),
-      title: Text('Learn ARF'),
-      content: SelectableText(
-          'LearnARF is a Flutter application which uses Google\'s ARCore to spawn 3D models into the real world. Additionally, it uses the Sceneform SDK to make it easier to handle 3D models.\n\nGithub Repository\ngithub.com/akshansh2000/learn_ar_flutter', textAlign: TextAlign.center,),
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+      ),
+      title: Text(
+        'LearnARF',
+        textAlign: TextAlign.center,
+      ),
+      content: Text(
+        strings.aboutApp,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/strings.dart
+++ lib/strings.dart
@@ -3,3 +3,7 @@ String statuePrefab = "statueModel";
 String cubePrefab = "cubeModel";
 String catPrefab = "catModel";
 String legoHousePrefab = "legoHouseModel";
+String aboutApp = "LearnARF is a Flutter application which uses "
+    "Google's ARCore to spawn 3D models into the real world. "
+    "Additionally, it uses the Sceneform SDK to make it easier "
+    "to handle 3D models.";
